7028a1c3046db44c2858366a8be9a2e7240f9cd2
youssef <youssef1.zakraoui@epitech.eu>
2021-07-16 17:58:14 +0200

    feat: add background

--- src/background.ts
+++ src/background.ts
@@ -1,8 +1,8 @@
 //Use to tell to get chrome if it 
 /// <reference types="chrome"/>
 import ImageCropper from "./components/ImageCropper";
-import { DataMessage } from "./models/DataMessage";
-import { Selection } from "./models/Selection";
+import {DataMessage} from "./models/DataMessage";
+import {Selection} from "./models/Selection";
 import API from "./components/api";
 
 console.log("Run tests")
@@ -15,7 +15,7 @@ let options: {
     preview: boolean,
     retrivePolice: boolean,
     retriveFormat: boolean,
-    translate: string|null
+    translate: string | null
 } = {
     preview: false,
     retrivePolice: false,
@@ -25,39 +25,39 @@ let options: {
 
 //Load config
 const savedConf = localStorage.getItem("options")
-if(savedConf) options = JSON.parse(savedConf);
+if (savedConf) options = JSON.parse(savedConf);
 
 //Listen command keys
-chrome.commands.onCommand.addListener( async(command: string) => {
+chrome.commands.onCommand.addListener(async (command: string) => {
     //Get active tab
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
         //TODO Verify if it's possible to have multiple active tabs
         let tab = tabs[0];
-        if(tab && tab.id){
-            if(command == "take-screenshot"){
+        if (tab && tab.id) {
+            if (command == "take-screenshot") {
                 //Tell the page script ta make a screenshot
-                chrome.tabs.sendMessage(tab.id, {msg:"screenshot-selection", tabId: tab.id});
-            }else if(command == "screenshot-selection-with-options"){
+                chrome.tabs.sendMessage(tab.id, {msg: "screenshot-selection", tabId: tab.id});
+            } else if (command == "screenshot-selection-with-options") {
                 //TODO
                 console.log("take screenshot with options")
 
             }
-        }else{
+        } else {
             throw Error("Can't find active tab !")
         }
     })
 });
 
 //Responce of selection call
-chrome.runtime.onMessage.addListener( (msg: DataMessage<Selection>)=>{
+chrome.runtime.onMessage.addListener((msg: DataMessage<Selection | NotificationOptions>, sender, response) => {
     //if it's the result of a selection then
-    if(msg.msg == "screenshot-selection-result"){
-        
+    if (msg.msg == "screenshot-selection-result") {
+
         //make a screenshoot
-        chrome.tabs.captureVisibleTab({format:"png"}, async (responce)=>{
+        chrome.tabs.captureVisibleTab({format: "png"}, async (responce) => {
             //Crop the image according to the selection
-            const croppedImageData = await ImageCropper.cropImage(responce,msg.data);
-            if(croppedImageData){
+            const croppedImageData = await ImageCropper.cropImage(responce, msg.data as Selection);
+            if (croppedImageData) {
                 console.log(croppedImageData)
                 //TODO api url and stuff with result
                 const apiResult = await API.getTextFromImage(croppedImageData);
@@ -65,5 +65,9 @@ chrome.runtime.onMessage.addListener( (msg: DataMessage<Selection>)=>{
 
         });
 
+    }
+    if (msg.msg === 'notification') {
+        chrome.notifications.create('', msg.data as NotificationOptions);
+        response(true)
     }
 })
