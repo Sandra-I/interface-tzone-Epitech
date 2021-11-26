59f12442360ceedd83ae1d3b12e0cb42141632ac
youssef <youssef1.zakraoui@epitech.eu>
2021-11-26 17:33:50 +0100

    style: use correct indentation

--- src/content.ts
+++ src/content.ts
@@ -4,36 +4,45 @@
 // This file is injected as a content script
 console.log("Hello from content script!")
 import Selector from "./components/selector"
-import { DataMessage, MessageType } from "./models/DataMessage";
-import { Selection } from "./models/Selection";
+import {DataMessage, MessageType} from "./models/DataMessage";
+import {Selection} from "./models/Selection";
 import Swal from "sweetalert2"
 import Popup from "./components/popup";
 import "./App.scss"
-import { UnknowMessageError } from "./errors/unknowMessageError";
-import { APIResponce } from "./models/apiResponce";
-import { APIResponceWithTraduction } from "./models/apiResponceWithTraduction";
+import {UnknowMessageError} from "./errors/unknowMessageError";
+import {APIResponce} from "./models/apiResponce";
+import {APIResponceWithTraduction} from "./models/apiResponceWithTraduction";
+
 const selector = new Selector();
 
 let isCurrentlySelection = false;
 
-chrome.runtime.onMessage.addListener( async(dataMsg: DataMessage<any>, sender, sendResponce) => {
-    if(isCurrentlySelection){
+chrome.runtime.onMessage.addListener(async (dataMsg: DataMessage<any>, sender, sendResponce) => {
+    if (isCurrentlySelection) {
         console.error("Already selecting an area !")
-    }else{
-        switch(dataMsg.msg){
+    } else {
+        switch (dataMsg.msg) {
             case MessageType.SCREENSHOT_SELECTION:
                 isCurrentlySelection = true;
-                const selection = selector.select().subscribe( (rec: Selection)=>{
+                const selection = selector.select().subscribe((rec: Selection) => {
                     isCurrentlySelection = false
                     selection.unsubscribe();
                     addLoadingAnimation();
-                    setTimeout( ()=> chrome.runtime.sendMessage( {msg: MessageType.SCREENSHOT_SELECTION_RESULT, data: rec, tabId: dataMsg.tabId}), 10);
+                    setTimeout(() => chrome.runtime.sendMessage({
+                        msg: MessageType.SCREENSHOT_SELECTION_RESULT,
+                        data: rec,
+                        tabId: dataMsg.tabId
+                    }), 10);
                 });
                 break;
             case MessageType.API_ERROR:
                 removeLoadingAnimation();
                 removePopup();
-                Swal.fire({"icon": "error", title:"API Error", text: "Impossible de convertire en text la capture : "+dataMsg.data?.message});
+                Swal.fire({
+                    "icon": "error",
+                    title: "API Error",
+                    text: "Impossible de convertire en text la capture : " + dataMsg.data?.message
+                });
                 break;
             case MessageType.SHOW_PREVIEW:
                 showPopup(dataMsg.data);
@@ -52,9 +61,9 @@ chrome.runtime.onMessage.addListener( async(dataMsg: DataMessage<any>, sender, s
 
 });
 
-function addLoadingAnimation(){
+function addLoadingAnimation() {
     const existingLoader = document.getElementById("TZone-loader")
-    if(!existingLoader){
+    if (!existingLoader) {
         let loaderContainer = document.createElement("div")
         loaderContainer.id = "TZone-loader";
         loaderContainer.style.position = "fixed";
@@ -72,12 +81,12 @@ function addLoadingAnimation(){
         loaderContainer.style.fontFamily = "arial";
 
         let loadingAnimation = document.createElement("img")
-        loadingAnimation.setAttribute("src", chrome.runtime.getURL("tzone-loader.svg") );
+        loadingAnimation.setAttribute("src", chrome.runtime.getURL("tzone-loader.svg"));
         loadingAnimation.style.width = "60px"
         loadingAnimation.style.height = "60px"
         loadingAnimation.style.display = "block"
         loaderContainer.appendChild(loadingAnimation)
-        
+
         let textLoading = document.createElement("div")
         textLoading.innerText = "Loading"
         loaderContainer.appendChild(textLoading)
@@ -90,43 +99,43 @@ function addLoadingAnimation(){
     }
 }
 
-function removeLoadingAnimation(){
+function removeLoadingAnimation() {
     const existingLoader = document.getElementById("TZone-loader")
-    if(existingLoader) existingLoader.remove();
+    if (existingLoader) existingLoader.remove();
 }
 
-function removePopup(){
+function removePopup() {
     const existingElement = document.getElementById("tzone-preview");
-    if(existingElement) existingElement.remove();
+    if (existingElement) existingElement.remove();
 }
 
-function showPopup(data: APIResponce){
+function showPopup(data: APIResponce) {
     removePopup();
-    const htlm = 
-    `<div style="width: 320px;margin: 5px;">`+
-        `<h2 class="tz-title">Texte copié</h2><br>`+
-        `<textarea style="resize: none;overflow: auto;width: 320px;background-color: white; color: black;" cols="40" rows="5" class="tz-result-text" readonly>`+
-            `${data.text}`+
-        `</textarea>`+
-    `</div>`;
-    const popup = new Popup("tzone-preview",htlm, { timeout: 2, fadeTime: 3 } );
+    const htlm =
+        `<div style="width: 320px;margin: 5px;">` +
+        `<h2 class="tz-title">Texte copié</h2><br>` +
+        `<textarea style="resize: none;overflow: auto;width: 320px;background-color: white; color: black;" cols="40" rows="5" class="tz-result-text" readonly>` +
+        `${data.text}` +
+        `</textarea>` +
+        `</div>`;
+    const popup = new Popup("tzone-preview", htlm, {timeout: 2, fadeTime: 3});
     popup.show();
 }
 
-function showPopupWithTranslation(data: APIResponceWithTraduction){
+function showPopupWithTranslation(data: APIResponceWithTraduction) {
     removePopup();
-    const htlm = 
-    `<div style="width: 320px;margin: 5px;">`+
-        `<h2 class="tz-title">Résultat</h2><br>`+
-        `<b>Texte original (${data.original.lang}):</b><br>`+
-        `<p style="overflow: auto;width: 320px;" class="tz-result-text">`+
-            `${data.original.text}`+
-        `</p><br><br>`+
-        `<b>Texte traduit (${data.translated.lang}):</b><br>`+
-        `<p style="overflow: auto;width: 320px;" class="tz-result-text">`+
-            `${data.translated.text}`+
-        `</textarea>`+
-    `</div>`;
-    const popup = new Popup("tzone-preview",htlm, { buttons: [{actionType: "exit", name: "Ok"}]} )
+    const htlm =
+        `<div style="width: 320px;margin: 5px;">` +
+        `<h2 class="tz-title">Résultat</h2><br>` +
+        `<b>Texte original (${data.original.lang}):</b><br>` +
+        `<p style="overflow: auto;width: 320px;" class="tz-result-text">` +
+        `${data.original.text}` +
+        `</p><br><br>` +
+        `<b>Texte traduit (${data.translated.lang}):</b><br>` +
+        `<p style="overflow: auto;width: 320px;" class="tz-result-text">` +
+        `${data.translated.text}` +
+        `</textarea>` +
+        `</div>`;
+    const popup = new Popup("tzone-preview", htlm, {buttons: [{actionType: "exit", name: "Ok"}]})
     popup.show();
 }
