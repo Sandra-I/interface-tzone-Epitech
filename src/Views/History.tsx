1324d7e0786a8bcc57deb501263fb46db35f3b52
Sandra-I <sandra.ibrahim@hotmail.fr>
2022-02-11 15:46:37 +0100

    Chore: fix button type

--- src/Views/History.tsx
+++ src/Views/History.tsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 const History = () => (
   <>
     <h3 className="tz-title">Historique</h3>
-    <Link to="/popup.html"><button>Retour</button></Link>
+    <Link to="/popup.html"><button type="submit">Retour</button></Link>
   </>
 );
 export default History;
