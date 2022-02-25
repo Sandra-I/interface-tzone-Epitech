f95cd2d5edd3164b29149e202ce339648347a468
Sandra-I <sandra.ibrahim@hotmail.fr>
2022-02-25 12:40:40 +0100

    Chore: Refacto

--- src/components/SwitchCheckbox.tsx
+++ src/components/SwitchCheckbox.tsx
@@ -1,79 +1,72 @@
 import * as React from 'react';
+import {
+  ChangeEvent,
+  useEffect,
+  useState,
+} from 'react';
 import './SwitchCheckbox.scss';
 import OptionsService from './optionsService';
 
-export default class SwitchCheckbox extends React.Component {
-  // TODO change default options place to Background.ts
-  options = new Map([
+const SwitchCheckbox: React.FC = () => {
+  const [checkOptions, setCheckOptions] = useState(new Map([
     ['preview', { name: 'Prévisualisation', check: false }],
-  ]);
+  ]));
 
-  state: {checkOptions: Map<string, {name: string, check: boolean}>};
-
-  constructor(props: any) {
-    super(props);
-
-    this.state = { checkOptions: this.options };
-
+  useEffect(() => {
     const optionsString = localStorage.getItem('options');
     if (optionsString) {
       const options = JSON.parse(optionsString);
-      Array.from(this.options.keys()).forEach((key) => {
-        const value: any = this.options.get(key);
-        this.options.set(key, { name: value.name, check: options.checkOptions[key] });
+      Array.from(checkOptions.keys()).forEach((key) => {
+        const value: any = checkOptions.get(key);
+        checkOptions.set(key, { name: value.name, check: options.checkOptions[key] });
       });
     }
-    this.handleChange = this.handleChange.bind(this);
-    this.setState({ checkOptions: this.options });
-  }
+    setCheckOptions(checkOptions);
+  }, []);
 
-  private handleChange(e: any, state: any) {
-    const { id } = e.target;
-    const newValue: any = state;
-    const changeField = newValue.get(id);
-    newValue.set(id, { name: changeField.name, check: e.target.checked });
-    this.setState({ checkOptions: newValue });
-
-    SwitchCheckbox.setOption(newValue);
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const { id, checked: check } = e.target;
+    setCheckOptions(checkOptions.set(id, { name: checkOptions.get(id)!.name, check }));
     chrome.storage.local.set({ [id]: e.target.checked });
     // Transforming the map into array is require, otherwise it will be empty for most data storage/manipulation
-  }
+  };
 
-  private static setOption(optionsMap: Map<string, any>) {
+  const setOption = (optionsMap: Map<string, any>) => {
     OptionsService.getOptions().then((options) => {
-      const checkOptions: any = {};
+      const optionsToSave: {[key: string]: boolean} = {};
       Array.from(optionsMap.keys()).forEach((key) => {
-        checkOptions[key] = optionsMap.get(key).check;
+        optionsToSave[key] = optionsMap.get(key).check;
       });
-      OptionsService.updateOptions({ ...options, checkOptions });
+      OptionsService.updateOptions({ ...options, checkOptions: optionsToSave as any });
     });
-  }
+  };
 
-  render() {
-    const { checkOptions } = this.state;
-    return (
-      <>
-        { Array.from(checkOptions.keys()).map((key: any) => (
-          <div key={key}>
-            <label htmlFor={key}>{checkOptions.get(key)?.name}</label>
-            <input
-              name={key}
-              checked={checkOptions.get(key)?.check}
-              onChange={(e) => this.handleChange(e, checkOptions)}
-              className="react-switch-checkbox"
-              id={key}
-              type="checkbox"
-            />
-            <label
-              className={`react-switch-label ${checkOptions.get(key)?.check ? 'background-color-of-switch' : ''}`}
-              htmlFor={key}
-            >
-              {/* Flo s'en occupe */}
-              <span className="react-switch-button" />
-            </label>
-          </div>
-        ))}
-      </>
-    );
-  }
-}
+  useEffect(() => setOption(checkOptions), [checkOptions]);
+
+  return (
+    <>
+      { Array.from(checkOptions.keys()).map((key: any) => (
+        <div key={key}>
+          <label htmlFor={key}>{checkOptions.get(key)!.name}</label>
+          <input
+            name={key}
+            checked={checkOptions.get(key)!.check}
+            onChange={(e) => handleChange(e)}
+            className="react-switch-checkbox"
+            id={key}
+            type="checkbox"
+          />
+          <label
+            className={`react-switch-label ${checkOptions.get(key)!.check ? 'background-color-of-switch' : ''}`}
+            htmlFor={key}
+          >
+            {/* Flo s'en occupera */}
+            <span className="react-switch-button" />
+          </label>
+        </div>
+      ))}
+    </>
+  );
+};
+
+export default SwitchCheckbox;
