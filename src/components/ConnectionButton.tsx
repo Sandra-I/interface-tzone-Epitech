03b07223e168561072ad3d6fb4d2eacfb00dd238
Sandra-I <sandra.ibrahim@hotmail.fr>
2022-03-01 22:17:06 +0100

    Feat: Bouton de connexion

---
+++ src/components/ConnectionButton.tsx
@@ -0,0 +1,31 @@
+import * as React from 'react';
+import { useState } from 'react';
+import './ConnectionButton.scss';
+
+const ConnectionButton: React.FC = () => {
+  const [name, setName] = useState<string>('');
+  const [isConnected, setIsConnected] = useState<boolean>(false);
+
+  function connection() {
+    console.log('connection', isConnected);
+    setName('Toto ');
+    setIsConnected(true);
+  }
+
+  function deconnection() {
+    console.log('disconnection', isConnected);
+    setName('');
+    setIsConnected(false);
+  }
+
+  return (
+    <>
+      <button type="button" className="myButton" onClick={!isConnected ? connection : deconnection}>
+        {isConnected ? 'Déconnexion' : 'Connexion'}
+      </button>
+      <p>{name}</p>
+    </>
+  );
+};
+
+export default ConnectionButton;
