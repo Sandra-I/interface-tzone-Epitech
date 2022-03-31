5fa197ff92e45df2399d20bc43692178c9e573a6
Sandra-I <sandra.ibrahim@hotmail.fr>
2022-03-31 11:18:54 +0200

    Fix : Suite Review

--- src/components/AccountButton.tsx
+++ src/components/AccountButton.tsx
@@ -7,15 +7,13 @@ const AccountButton: React.FC<{name: string}> = ({ name }) => {
   }
 
   return (
-    <>
-      <div role="button" className="accountButton" onClick={showAccount} onKeyUp={showAccount} tabIndex={0}>
-        <p>
-          <a href="https://main.deqtbus9d66sw.amplifyapp.com/login" target="_blank" rel="noreferrer">
-            {name.toUpperCase()}
-          </a>
-        </p>
-      </div>
-    </>
+    <div role="button" className="accountButton" onClick={showAccount} onKeyUp={showAccount} tabIndex={0}>
+      <p>
+        <a href="https://main.deqtbus9d66sw.amplifyapp.com/login" target="_blank" rel="noreferrer">
+          {name.toUpperCase()}
+        </a>
+      </p>
+    </div>
   );
 };
 
