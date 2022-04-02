31ee04dc388d2ab453090197120c0ce949103ff6
fmo-ep <mouton.florent@epitech.eu>
2022-04-02 19:40:38 +0200

    add views tests

---
+++ src/tests/user-mock.ts
@@ -0,0 +1,41 @@
+import { User } from '../models/user';
+
+/** @ts-ignore */
+export const UserMockWithHistory: User = {
+  _id: '123',
+  firstName: 'Toto',
+  lastName: 'Pizza',
+  email: 'toto@yopmail.com',
+  isAdmin: false,
+  history: [{
+    text: 'Test',
+    /** @ts-ignore */
+    date: '08/11/2022',
+  }],
+  permissions: {
+    selection: true,
+    preview: true,
+    history: true,
+    translation: true,
+    capture: true,
+    quickCapture: true,
+  },
+};
+
+/** @ts-ignore */
+export const UserMockWithoutHistory: User = {
+  _id: '123',
+  firstName: 'Toto',
+  lastName: 'Pizza',
+  email: 'toto@yopmail.com',
+  isAdmin: false,
+  history: [],
+  permissions: {
+    selection: true,
+    preview: true,
+    history: true,
+    translation: true,
+    capture: true,
+    quickCapture: true,
+  },
+};
