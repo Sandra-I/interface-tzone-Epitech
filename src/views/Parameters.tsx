22fc45577f8b4da7b37c661cfcc3ea97aafb93ec
YamiKami-Sama <wouna@live.fr>
2022-04-02 12:34:36 +0200

    fix bad 'Parameters.tsx' merge

--- src/views/Parameters.tsx
+++ src/views/Parameters.tsx
@@ -3,21 +3,9 @@ import { Link } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
 import SwitchCheckbox from '../components/SwitchCheckbox';
 import Translate from '../components/Translate';
-import { MessageType } from '../models/DataMessage';
 import { User } from '../models/user';
 
 const Parameters: React.FC<{user: User}> = ({ user }) => {
-  function triggerSelectionEvent() {
-    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-      const tab = tabs[0];
-      if (tab && tab.id) {
-        // Tell the page script ta make a screenshot
-        chrome.tabs.sendMessage(tab.id, { msg: MessageType.SCREENSHOT_SELECTION, tabId: tab.id });
-      } else {
-        throw Error("Can't find active tab !");
-      }
-    });
-  }
   const { t } = useTranslation();
   return (
     <div className="card">
@@ -26,14 +14,17 @@ const Parameters: React.FC<{user: User}> = ({ user }) => {
         <SwitchCheckbox />
         { user?.permissions.translation && <Translate /> }
         <div>
-          <span>Raccourci clavier récupération :</span>
+          <span>
+            {t('keyShortcutScreenshot')}
+            {' '}
+            :
+          </span>
           <span className="short">ALT + S</span>
         </div>
       </div>
       { user?.permissions.history && <Link to="/history"><button type="button">Historique</button></Link> }
       {/* <Link to="/history"><button>Historique</button></Link> */}
       {/* <br /> */}
-      <button type="button" className="myButton" onClick={triggerSelectionEvent}>Faire une capture</button>
     </div>
   );
 };
